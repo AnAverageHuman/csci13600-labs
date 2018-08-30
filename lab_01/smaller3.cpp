e3cd91e10557942d002fda2eeb532b75ba587aa5
Jeffrey Lin <jeffrey@icurse.nl>
2018-08-30 11:58:05 -0400

    Add Lab 1, Task B

---
+++ lab_01/smaller3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Enter the first number: ";
+  int first;
+  std::cin >> first;
+
+  std::cout << "Enter the second number: ";
+  int second;
+  std::cin >> second;
+
+  std::cout << "Enter the third number: ";
+  int third;
+  std::cin >> third;
+
+  std::cout << "The smaller of the three is ";
+  std::cout << (first < second ?
+      (first < third ? first : third) :
+      (second < third ? second : third));
+  std::cout << std::endl;
+  return 0;
+}
+
