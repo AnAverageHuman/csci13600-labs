2b5d979eef76e9db200a9886acb0da2ea356d5ac
Jeffrey Lin <jeffrey@icurse.nl>
2018-09-29 20:43:18 -0400

    Add Lab 4, Task D

--- lab_04/include/main.hpp
+++ lab_04/include/main.hpp
@@ -5,12 +5,14 @@
 
 const char MUH_CHAR = '*';
 const char MUH_SPACE = ' ';
+const std::string MUH_SHAR = "*";
 const std::string MUH_CHAR_SPACE = "* ";
 const std::string MUH_SPACE_CHAR = " *";
 
 std::string box(int width, int height);
 std::string checkerboard(int width, int height);
 std::string cross(int size);
+std::string lower(int length);
 
 #endif // MAIN_HPP
 
