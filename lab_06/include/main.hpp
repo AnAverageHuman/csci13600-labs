9ccd8b57ee8ae24ac0e3722f5bc6a87b604cede2
Jeffrey Lin <jeffrey@icurse.nl>
2018-10-17 13:51:32 -0400

    Add Lab 6, Task B

--- lab_06/include/main.hpp
+++ lab_06/include/main.hpp
@@ -4,6 +4,8 @@
 #include <string>
 
 std::string test_ascii(std::string muhstring);
+char shiftChar(char c, int rshift);
+std::string encryptCaesar(std::string plaintext, int rshift);
 
 #endif // MAIN_HPP
 
