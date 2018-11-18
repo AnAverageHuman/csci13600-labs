59740171f8c6ecefc89c6ea2e50c3b4799cf9104
Jeffrey Lin <jeffrey@icurse.nl>
2018-11-17 21:12:40 -0500

    Add Lab 10, Task B

--- lab_10/src/time.cpp
+++ lab_10/src/time.cpp
@@ -11,3 +11,8 @@ int minutesUntil(Time earlier, Time later) {
 bool operator==(const Time a, const Time b) {
   return a.h == b.h && a.m == b.m;
 }
+
+Time addMinutes(Time time0, int min) {
+  int m = time0.m + min;
+  return Time{time0.h + m / 60, m % 60};
+}
