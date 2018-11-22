6e0bc553233297528429c82d04b14ad5fe0242f4
Jeffrey Lin <jeffrey@icurse.nl>
2018-11-22 15:36:34 -0500

    Add Lab 11, Task A

--- lab_11/include/main.hpp
+++ lab_11/include/main.hpp
@@ -1,5 +1,7 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+void printRange(int left, int right);
+
 #endif // MAIN_HPP
 
---
+++ lab_11/src/recursion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "main.hpp"
+
+void printRange(int left, int right) {
+  if (left > right) {
+    return;
+  }
+
+  std::cout << left;
+  std::cout << " ";
+  printRange(++left, right);
+}
