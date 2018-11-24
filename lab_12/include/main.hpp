6e2a791f06e1d2bcc4af7c28fdc8ac6f7324805a
Jeffrey Lin <jeffrey@icurse.nl>
2018-11-24 15:43:51 -0500

    Add Lab 12, Task A

--- lab_12/include/main.hpp
+++ lab_12/include/main.hpp
@@ -1,4 +1,18 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#include <string>
+
+class Profile {
+private:
+  std::string username;
+  std::string dispname;
+
+public:
+  Profile(std::string u = "", std::string d = "") : username(u), dispname(d){};
+  std::string getUsername() const { return username; };
+  std::string getFullName() const;
+  void setDisplayName(std::string d) { dispname = d; };
+};
+
 #endif // MAIN_HPP
